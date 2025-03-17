c80580fb3686c210a3efab6ca16f3e23b16eb234
Your Name <your_email@example.com>
2025-03-16 22:14:20 -0700

    india

--- types/generated/contentTypes.d.ts
+++ types/generated/contentTypes.d.ts
@@ -824,7 +824,7 @@ export interface ApiThirdnameThirdname extends Struct.CollectionTypeSchema {
   collectionName: 'thirdnames';
   info: {
     description: '';
-    displayName: 'Malaysia-Form';
+    displayName: 'India-Form';
     pluralName: 'thirdnames';
     singularName: 'thirdname';
   };
