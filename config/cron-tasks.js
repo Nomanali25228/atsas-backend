dcc48e97e71ef2f24e7460b806ba7275c14ef9fd
Your Name <your_email@example.com>
2025-04-23 17:46:55 -0700

    updat

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -199,7 +199,7 @@ module.exports = {
                 <span style="color: #003c71;"> Diplomatic </span> Simulation <span style="color: #003c71;"> of </span>
                 <br> <span style="color: #003c71;"> the </span> United Nations
               </p>
-              <div style="height: 440px;"></div>
+              <div style="height: 450px;"></div>
               <div style="width: 100%;">
                 <img
                   src="https://ftueyhj.stripocdn.email/content/guids/CABINET_76edfd603639686b19c3686766c1f4512d98005c2183ca057d0378867dab713b/images/whatsapp_image_20250409_at_111009_97810a2aremovebgpreview.png"
@@ -277,8 +277,8 @@ module.exports = {
                 <span style="color: #003c71;"> Diplomatic </span> Simulation <span style="color: #003c71;"> of </span>
                 <br> <span style="color: #003c71;"> the </span> United Nations
               </p>
-              <div style="height: 110px;"></div>
-              <div style="width: 100%; margin-top: 350px;">
+              <div style="height: 100px;"></div>
+              <div style="width: 100%; margin-top: 340px;">
                 <img
                   src="https://ftueyhj.stripocdn.email/content/guids/CABINET_76edfd603639686b19c3686766c1f4512d98005c2183ca057d0378867dab713b/images/whatsapp_image_20250409_at_111009_97810a2aremovebgpreview.png"
                   alt="Leaf" style="width:100%; display: block;" />
@@ -362,6 +362,7 @@ module.exports = {
 </body>
 
 </html>
+
 
 
 
