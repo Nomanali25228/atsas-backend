987dd7568635911a3729d857f01d66e1358918f3
Your Name <your_email@example.com>
2025-04-23 17:31:02 -0700

    updat

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -247,7 +247,7 @@ module.exports = {
                 </a>
               </p>
 
-              <p style="font-size: 10px; color: #666; text-align: center; margin-top: 250px;">
+              <p style="font-size: 10px; color: #666; text-align: center; margin-top: 280px;">
                 Address: 42 Heneretton Way High Wycombe, HP13 7UE, United Kingdom<br />
                 Website: www.atsasmun.com | Email: info@atsasmun.com<br />
                 Phone: +44 7487 302322
@@ -278,7 +278,7 @@ module.exports = {
                 <br> <span style="color: #003c71;"> the </span> United Nations
               </p>
               <div style="height: 100px;"></div>
-              <div style="width: 100%; margin-top: 280px;">
+              <div style="width: 100%; margin-top: 320px;">
                 <img
                   src="https://ftueyhj.stripocdn.email/content/guids/CABINET_76edfd603639686b19c3686766c1f4512d98005c2183ca057d0378867dab713b/images/whatsapp_image_20250409_at_111009_97810a2aremovebgpreview.png"
                   alt="Leaf" style="width:100%; display: block;" />
@@ -362,6 +362,7 @@ module.exports = {
 </body>
 
 </html>
+
 
 
         `;
