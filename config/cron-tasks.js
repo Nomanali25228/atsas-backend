54f4f44677a0d4ebd76eed2dfc4fb2e2df63383e
Noman Ali <nomi4698dg@gmail.com>
2025-11-22 09:58:50 +0500

    hell

--- config/cron-tasks.js
+++ config/cron-tasks.js
@@ -5,8 +5,7 @@ module.exports = {
   // CRON: Runs every minute
   '*/1 * * * *': async ({ strapi }) => {
     try {
-      const oneHourAgo =new Date(Date.now() - 1 * 60 * 1000);
-
+      const oneHourAgo =new Date(Date.now() - 8 * 60 * 60 * 1000);
 
       const notifications = await strapi.db.query('api::notification.notification').findMany({
         where: {
@@ -208,7 +207,7 @@ module.exports = {
                 <span style="color: #003c71;"> Diplomatic </span> Simulation <span style="color: #003c71;"> of </span>
                 <br> <span style="color: #003c71;"> the </span> United Nations
               </p>
-              <div style="height: 459px;"></div>
+              <div style="height: 457px;"></div>
               <div style="width: 100%;">
                 <img
                   src="https://ftueyhj.stripocdn.email/content/guids/CABINET_76edfd603639686b19c3686766c1f4512d98005c2183ca057d0378867dab713b/images/whatsapp_image_20250409_at_111009_97810a2aremovebgpreview.png"
